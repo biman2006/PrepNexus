ab399cfa2bd431a8f8fb1899cc16f08b4212a831
biman2006 <adhikarybiman511@gmail.com>
2026-04-28 20:27:38 +0530

    add all_skills

--- test.py
+++ test.py
@@ -4,12 +4,13 @@ from rag.embedder import load_embeddings
 from rag.vector_store import create_vectorestore 
 from data.job_roles import docs
 from rag.retriever import retrieve_role_info
-'''
+from utils.skill_extractor import extract_skill
+from data.all_skills import ALL_SKILLS
+
 resume_text=extract_text_from_pdf("sample_resume.pdf")
 cleand=clean_text(resume_text)
-print("ORIGINAL\n",resume_text[:100])
-print("CLEAND\n",cleand[:100])
-'''
+resume_skills=set(extract_skill(cleand,ALL_SKILLS))
+
 
 embeddings=load_embeddings() 
 vectorstore=create_vectorestore(docs,embeddings)
