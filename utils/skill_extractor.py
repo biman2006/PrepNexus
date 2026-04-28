83a8e2c40fb72eebc8f895f7727ecc76f0782764
biman2006 <adhikarybiman511@gmail.com>
2026-04-28 21:36:32 +0530

    Fix Error

--- utils/skill_extractor.py
+++ utils/skill_extractor.py
@@ -1,23 +1,38 @@
-def extract_skill(text,skill_list):
-    found_skill=[]
+import string
 
-    text=text.lower()
+def extract_skill(text, skill_list):
+    found_skill = []
 
-    words=set(text.split())
+    # Lowercase
+    text = text.lower()
 
-    sorted_skills=sorted(skill_list,key=len, reverse=True)
+    # Remove punctuation
+    text = text.translate(
+        str.maketrans('', '', string.punctuation)
+    )
+
+    # Tokenize
+    words = set(text.split())
+
+    # Sort longer skills first
+    sorted_skills = sorted(
+        skill_list,
+        key=len,
+        reverse=True
+    )
 
     for skill in sorted_skills:
-        skill_lower=skill.lower()
 
-        if " " in skill.lower():
+        skill_lower = skill.lower()
+
+        # Multi-word skill
+        if " " in skill_lower:
             if skill_lower in text:
                 found_skill.append(skill_lower)
 
+        # Single-word skill
         else:
             if skill_lower in words:
                 found_skill.append(skill_lower)
 
-
     return list(set(found_skill))
-        
